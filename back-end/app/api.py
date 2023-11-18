4b75e14d25736c6433b9a41b153a626ec278d8f9
MaxQuin1 <maximoquinteroescobar8@gmail.com>
2023-11-18 11:26:13 -0600

    El crud con FastApi y el front con chakra

---
+++ back-end/app/api.py
@@ -0,0 +1,92 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Define the cors origins
+origins = [
+    'http://localhost:3000',
+    'localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+# Get Route for testing
+
+
+@app.get('/', tags=['root'])
+async def read_root() -> dict:
+    return {'message': '¡Bienvenido a FastApi!'}
+
+todos = [
+    {
+        'id': '1',
+        'team': 'Futbol'
+    },
+    {
+        'id': '2',
+        'team': 'Basquetbol'
+    },
+    {
+        'id': '3',
+        'team': 'Tenis'
+    },
+    {
+        'id': '4',
+        'team': 'Natacion'
+    }
+]
+
+# Get todos route
+
+
+@app.get('/todo', tags=['todos'])
+async def get_todos() -> dict:
+    return {'data': todos}
+
+# Post route
+
+
+@app.post('/todo', tags=['todos'])
+async def add_todo(todo: dict) -> dict:
+    todos.append(todo)
+    return {
+        'data': {'¡todo has been added!'}
+    }
+
+# PUT ROUTE
+
+
+@app.put("/todo/{id}", tags=["todos"])
+async def update_todo(id: int, body: dict) -> dict:
+    for todo in todos:
+        if int(todo["id"]) == id:
+            todo["team"] = body["team"]
+            return {
+                "data": f"Todo with id {id} has been updated."
+            }
+
+    return {
+        "data": f"Todo with id {id} not found."
+    }
+
+# Delete Route
+
+
+@app.delete('/todo/{id}', tags=['todos'])
+async def delete_todo(id: int) -> dict:
+    for todo in todos:
+        if int(todo['id']) == id:
+            todos.remove(todo)
+            return {
+                'data': f'Todo with id {id} has been removed!'
+            }
+    return {
+        'data': f'Todo with id {id} is not found!'
+    }
---
+++ front-end/frontend/src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {Heading, flex, Divider, Flex} from 
+'@chakra-ui/react'
+
+const Header = () =>{
+    return (
+        <Flex
+        as='nav'
+        align='center'
+        justify='space-between'
+        wrap='wrap'
+        paddingY='1rem'
+        paddingX='1rem'
+        bg='twitter.500'
+        color='white'
+        boxShadow='0px 2px 4px rgba(0,0,0.2)'>
+            <Flex align='center' mr={5}>
+                <Heading
+                as='h1'
+                size='lg'
+                fontWeight='bold'
+                >
+                    Task Manager
+                </Heading>
+            </Flex>
+        </Flex>
+    )
+}
+
+export default Header;
